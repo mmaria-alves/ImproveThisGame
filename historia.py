30b5bb0fe12873aec014a3f2b8174ed9a0111646
Maria Eduarda <mariaeduardaalves0615@gmail.com>
2025-07-17 22:25:57 -0300

    Update historia.py

--- historia.py
+++ historia.py
@@ -2,31 +2,18 @@ from utils import Utils
 
 def mostrar_introducao():
     Utils.limpar_tela()
-    historia = [
-        "A Terra foi atacada e agora está lotada de seres paranormais,",
-        "ameaçando a ordem e o equilíbrio do planeta.",
-        "",
-        "Dentre esses seres, existem os System Boys, um grupo que tem como",
-        "principal objetivo instaurar ainda mais caos ao redor do globo.",
-        "",
+    introducao = [
+        "A Terra foi atacada e agora está lotada de seres paranormais, ameaçando a ordem e o equilíbrio do planeta.",
+        "Dentre esses seres, existem os System Boys, um grupo que tem como principal objetivo instaurar ainda mais caos\nao redor do globo.",
         "Entretanto, é claro que nosso mundo não ficou à mercê dos vilões.",
-        "",
-        "Um grupo de amigas sofreu um acidente com a queda das naves",
-        "extraterrestres no mundo e acabaram se perdendo por dias numa floresta.",
-        "",
-        "Sem escolha, Diana, Lara, Beatrice e Luna se estabelecem naquele bosque.",
-        "",
-        "Depois de comer uma fruta com aparência e gosto estranho,",
-        "as quatro desenvolvem poderes.",
-        "",
-        "Ao ver o que havia acontecido com o resto do planeta,",
-        "elas adotam o nome de TAKEDOWN.",
-        "",
-        "Agora, o objetivo principal delas é salvar as pessoas restantes,",
-        "enquanto tentam entender e se acostumar com suas novas habilidades.",
-        "",
+        "..."
+        "Um grupo de amigas sofreu um acidente com a queda das naves extraterrestres no mundo e acabaram ficando presas\npor dias numa floresta.",
+        "Sem escolha, Diana, Lara, Beatrice e Luna se estabeleceram naquele bosque enquanto esperavam pelo resgate.",
+        "Mas, depois de comer uma fruta com aparência e gosto estranho, as quatro desenvolvem poderes e foram capazes\nde sair, por conta própria, daquela matagal.",
+        "E, ao ver o que havia acontecido com o resto do planeta, elas decidiram usar seus poderes para salvar as pessoas\nque restaram",
+        "Adotando o nome de TAKEDOWN, tudo isso enquanto tentam entender e se acostumar com suas novas habilidades.",
         "Prepare-se para entrar nessa aventura!"
     ]
-    for trecho in historia:
+    for trecho in introducao:
         print(trecho)
-        input('Pressione ENTER para continuar...')
+        input('Pressione ENTER para continuar...\n')
